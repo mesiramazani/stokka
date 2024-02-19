5fd0bf34acc99326997af1f8e71658a93d17a2be
mesiramazani <ramazanimeissam@gmail.com>
2024-02-19 23:43:54 +0100

    removed variable

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
-    uint256 public number3;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
