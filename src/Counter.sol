e597e21f00503c0bcf4fca0b3849ec389370da52
mesiramazani <ramazanimeissam@gmail.com>
2024-02-19 23:34:21 +0100

    changed variable

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 contract StokkaCDP {
     uint256 public number;
+    uint256 public number2;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
