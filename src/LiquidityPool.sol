152568c7419fccf5e01556527d5a8cae8d44b8ae
mesiramazani <ramazanimeissam@gmail.com>
2024-02-23 09:47:15 +0100

    Create LiquidityPool.sol

---
+++ src/LiquidityPool.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+/**
+@title: LiquidityPool
+@author: The Stokka Team
+@notice: The following contract is a Liquidity Pool for the sAAPL token and ETH.
+
+@notice: The prices in the liquidity pool are determined by the ratio of the two assets in the pool.
+*/
+
+pragma solidity ^0.8.19;
+
+contract LiquidityPool {
+    constructor() {}
+}
